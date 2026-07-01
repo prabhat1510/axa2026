0762d7c9030543c99c624fcd95146913ee0a81b8
prabhat1510 <prabhatc@gmail.com>
2026-07-01 12:16:59 +0530

    All hands on

---
+++ webpageexample/hello.js
@@ -0,0 +1,7 @@
+function validate(){
+    let firstName= document.getElementById("fname").value
+    let password= document.getElementById("pass").value
+    console.log(firstName,password) //this code will display user entered value on browser console
+    //We have send firstName and password for validation at backend 
+
+}
